4b969819f9ae16635d692980835c12307ab8ff53
Unnawut Leepaisalsuwanna <unnawut@omise.co>
2019-07-04 22:55:54 +0700

    Remove extra comma

--- apps/potterhat_rpc/config/config.exs
+++ apps/potterhat_rpc/config/config.exs
@@ -1,6 +1,6 @@
 use Mix.Config
 
 config :potterhat_rpc,
-  rpc_port: {:system, "RPC_PORT", 8545, {String, :to_integer}},
+  rpc_port: {:system, "RPC_PORT", 8545, {String, :to_integer}}
 
 import_config "#{Mix.env()}.exs"
