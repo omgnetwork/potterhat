e76f6301bbfc22a7fcf4ead25805f533100e2346
Unnawut Leepaisalsuwanna <unnawut@omise.co>
2019-07-30 23:02:43 +0700

    Add :potterhat subapp which is being called by the library user

---
+++ apps/potterhat/mix.exs
@@ -0,0 +1,50 @@
+# Copyright 2019 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule Potterhat.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :potterhat,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.8",
+      elixirc_paths: elixirc_paths(Mix.env),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    []
+  end
+
+  # Specifies which paths to compile per environment.
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
+
+  defp deps do
+    [
+      {:potterhat_elixir, in_umbrella: true}
+      {:potterhat_metrics, in_umbrella: true}
+      {:potterhat_node, in_umbrella: true}
+      {:potterhat_rpc, in_umbrella: true}
+      {:potterhat_utils, in_umbrella: true}
+    ]
+  end
+end
